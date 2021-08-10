1d65c6d65d3a9968d68dc2bbd964a76ed56aaebc
abhinav04-2 <87425892+abhinav04-2@users.noreply.github.com>
2021-08-10 12:47:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+function preload(){
+  add createImageBitmap=powerimg//pre-load images
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(22);
+
+}
